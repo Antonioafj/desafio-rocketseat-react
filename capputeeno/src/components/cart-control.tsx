58669c593cde0daf102a188f602771beeaf51176
Antonioafj <antonioafj.edu@gmail.com>
2025-01-08 20:35:45 -0400

    feat: creating header

--- capputeeno/src/components/cart-control.tsx
+++ capputeeno/src/components/cart-control.tsx
@@ -1,9 +1,31 @@
+import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { CartIcon } from "./cart-icon";
+import {styled} from "styled-components";
 
-export function CortControl(){
+const CartCount = styled.span`
+    width: 17px;
+    height: 17px;
+    border-radius: 100%;
+    padding: 0px 5px;
+    font-size: 10px;
+
+    background-color: var(--delete-color);
+    color: white;
+
+    margin-left: -10px;
+`
+
+const Container = styled.div`
+    position: relative;
+`
+
+export function CartControl(){
+    const {value} = useLocalStorage( 'cart-items ')
+    
     return (
-        <div>
+        <Container>
             <CartIcon/>
-        </div>
+            {value.length && <CartCount>{value.length}</CartCount>}
+        </Container>
     )
 }
--- capputeeno/src/components/header.tsx
+++ capputeeno/src/components/header.tsx
@@ -3,6 +3,7 @@
 import styled from "styled-components"
 import {Saira_Stencil_One} from "next/font/google";
 import { PrimaryInputWSearchIcon } from "./primary-input";
+import { CartControl } from "./cart-control";
 
 
 const sairaStencil = Saira_Stencil_One({
@@ -20,7 +21,16 @@ const TagHeader = styled.header`
     align-items: center;
     justify-content: space-between;
     paddind: 20px 160px;
+
+    
+    > div {
+        display: flex;
+        align-items= center;
+        justify-content: center;
+        gap: 24px;
+    }    
 `
+
 const Logo = styled.a `
     color: var(--logo-color);
     font-weight: 400;
@@ -35,6 +45,7 @@ export function Header(props : HeaderProps){
             <Logo className={sairaStencil.className}>Capputeeno</Logo>
             <div>
                 <PrimaryInputWSearchIcon placeholder="Procurando por algo específico?" />
+                <CartControl/>
             </div>
         </TagHeader>
     )
